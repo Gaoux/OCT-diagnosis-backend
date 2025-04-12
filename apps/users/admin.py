c3c987575951c22aae633f56ded1d619c65b16ef
serght <sergioherrerat@javeriana.edu.co>
2025-04-12 11:50:24 -0500

    :zap: The admin is automatically created, the login and register URLs are configured, and it is now fully functional.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -4,12 +4,13 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_superuser', 'is_active')
     fieldsets = UserAdmin.fieldsets + (
-        ('Rol Personalizado', {'fields': ('role',)}),
+        ('Información adicional', {'fields': ('name', 'role', 'is_admin')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Rol Personalizado', {'fields': ('role',)}),
+        ('Información adicional', {'fields': ('name', 'role', 'is_admin')}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ
 import os
+from corsheaders.defaults import default_headers
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -169,3 +172,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom User (required for login/register)
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# CORS CONFIG PARA FRONTEND
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'X-CSRFToken',
+    'authorization',
+]
