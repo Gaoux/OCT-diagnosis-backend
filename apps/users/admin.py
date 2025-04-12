76b8c2c14c977dc98e26e7c427137a6d9b5cd48c
serght <sergioherrerat@javeriana.edu.co>
2025-04-12 10:27:19 -0500

    :zap:refactor: adjusted user model and serializers for role management
    - Removed 'profession' field from CustomUser model.
    - Added 'role' field with choices: paciente, oftalmologo, admin.
    - Updated serializers to support the new 'role' field during registration and serialization.
    - Ensured role-based data is stored and retrieved correctly for frontend use.
    - new migrations
    - admin.py creation

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Rol Personalizado', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Rol Personalizado', {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
