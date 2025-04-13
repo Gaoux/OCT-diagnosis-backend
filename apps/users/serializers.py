f2a4495cb44624cbfff976c1ad9a980125957909
jtommy08 <jtommy08@hotmail.com>
2025-04-13 18:42:15 -0500

    Implementación de CRUD y dashboard admin

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -21,3 +21,52 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'name', 'role', 'is_admin')
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    """Serializer para ver detalles completos de un usuario"""
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'name', 'role', 'is_admin', 'date_joined')
+        read_only_fields = ('date_joined',)
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    """Serializer para crear usuarios desde el panel de administración"""
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password', 'name', 'role', 'is_admin')
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser.objects.create_user(
+            password=password,
+            **validated_data
+        )
+        return user
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    """Serializer para actualizar usuarios"""
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'name', 'role', 'is_admin')
+
+class AdminRegistrationSerializer(serializers.ModelSerializer):
+    """Serializer específico para crear administradores"""
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password', 'name')
+    
+    def create(self, validated_data):
+        validated_data['role'] = 'admin'
+        validated_data['is_admin'] = True
+        return CustomUser.objects.create_user(**validated_data)
+
+class DashboardStatsSerializer(serializers.Serializer):
+    """Serializer para las estadísticas del dashboard"""
+    total_users = serializers.IntegerField()
+    total_patients = serializers.IntegerField()
+    total_ophthalmologists = serializers.IntegerField()
+    total_admins = serializers.IntegerField()
